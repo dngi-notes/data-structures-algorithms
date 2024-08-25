ca9bffb2fd829c378dc6876c46406f00eabcd6ed
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-25 10:46:32 +0100

    lc 1768 solution

---
+++ leetcode-75/lc1768-merge-strings-alternatively.java
@@ -0,0 +1,39 @@
+/**
+ * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+ * If a string is longer than the other, append the additional letters onto the end of the merged string.
+ * Return the merged string.
+ * 
+ */
+class Solution {
+    public String mergeAlternatively(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        int len1 = word1.length();
+        int len2 = word2.length();
+
+        int i = 0;
+        int j = 0;
+
+        while(i < len1 || j < len2) {
+            if(i < len1) {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+
+            if(j < len2) {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String word1 = "abc";
+        String word2 = "pqr";
+
+        System.out.println(sol.mergeAlternatively(word1, word2));
+    }
+}
