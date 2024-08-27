077cb3eb131531d7a6cef2bdff3d96246688af2c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-27 19:42:37 +0100

    lc151 attempt solution

--- leetcode-75/arrays_strings/lc151_reverse_words_in_string/ReverseWords.java
+++ leetcode-75/arrays_strings/lc151_reverse_words_in_string/ReverseWords.java
@@ -2,13 +2,29 @@ package arrays_strings.lc151_reverse_words_in_string;
 
 public class ReverseWords {
     /*
-     Given an input string s, reverse the order of the words.
-     A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
-     Return a string of the words in reverse order concatenated by a single space.
-     Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
-    */
+     * Given an input string s, reverse the order of the words.
+     * A word is defined as a sequence of non-space characters. The words in s will
+     * be separated by at least one space.
+     * Return a string of the words in reverse order concatenated by a single space.
+     * Note that s may contain leading or trailing spaces or multiple spaces between
+     * two words. The returned string should only have a single space separating the
+     * words. Do not include any extra spaces.
+     */
 
     public String reverseWords(String s) {
-        
+        String[] words = s.trim().split("\\s+");
+
+       StringBuilder sb = new StringBuilder();
+       for(int i = words.length - 1; i > 0; i--) {
+        sb.append(words[i]).append(" ");
+       }
+       return sb.toString() + words[0];
+}
+
+    public static void main(String[] args) {
+        ReverseWords reverseWords = new ReverseWords();
+
+        String s = "the sky is blue";
+        System.out.println(reverseWords.reverseWords(s));
     }
 }
