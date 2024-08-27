3911e41364c0d775ba2a817f6959e8f4efa230e2
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-27 02:22:30 +0100

    added first edge case

--- leetcode-75/arrays_strings/lc_605_can_place_flowers/CanPlaceFlowers.java
+++ leetcode-75/arrays_strings/lc_605_can_place_flowers/CanPlaceFlowers.java
@@ -27,14 +27,19 @@ public class CanPlaceFlowers {
     public static void main(String[] args) {
         CanPlaceFlowers sol = new CanPlaceFlowers();
 
-        /* Case 1: Placing One in The Middle: Expected Output - True */ 
+        /* Case 1 - Expected Output: true */ 
         int[] input1 = {1, 0, 0, 0, 1};
         int n1 = 1;
         System.out.println(sol.canPlaceFlowers(input1, n1));
 
-        /* Case 2: n = 2 */
+        /* Case 2 - Expected Output: false */
         int[] input2 = {1, 0, 0, 0, 1};
         int n2 = 2;
-        System.out.println(sol.canPlaceFlowers(input2, n2)); 
+        System.out.println(sol.canPlaceFlowers(input2, n2));
+
+        /* Edge Case 1: empty array and n = 0 - Expected Output: true*/
+        int[] input3 = {};
+        int n3 = 0;
+        System.out.println(sol.canPlaceFlowers(input3, n3));
     }
 }
