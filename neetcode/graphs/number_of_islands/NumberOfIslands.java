92304a1fccdf44d7a9f54d67cbea09118ee4119b
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-28 22:15:06 +0100

    accounted for case where the length of the array is 0 or if the array is null

--- neetcode/graphs/number_of_islands/NumberOfIslands.java
+++ neetcode/graphs/number_of_islands/NumberOfIslands.java
@@ -10,6 +10,10 @@ public class NumberOfIslands {
      */
 
     public int numIslands(char[][] grid) {
+        if(grid.length == 0 || grid == null) {
+            return 0;
+        }
+
         int row = grid.length;
         int col = grid[0].length;
 
@@ -53,6 +57,62 @@ public class NumberOfIslands {
                 { '0', '0', '0', '0', '0' }
         };
         System.out.println(noi.numIslands(grid1));
+
+        /* Edge Case 1 - Empty Grid - Expected Output: 0 */
+        char[][] grid2 = {};
+        System.out.println(noi.numIslands(grid2));
+
+        /* Edge Case 2 - Single Element Grid - Expected Output: 0 */
+        char[][] grid3 = { { '0' } };
+        System.out.println(noi.numIslands(grid3));
+
+        /* Edge Case 2 - Single Element Grid - Expected Output: 1 */
+        char[][] grid4 = { { '1' } };
+        System.out.println(noi.numIslands(grid4));
+
+        /* Edge Case 3 - All Water - Expected Output: 0 */
+        char[][] grid5 = {
+                { '0', '0', '0' },
+                { '0', '0', '0' },
+                { '0', '0', '0' }
+        };
+        System.out.println(noi.numIslands(grid5));
+
+        /* Edge Case 4 - All Land - Expected Output: 1 */
+        char[][] grid6 = {
+                { '1', '1', '1' },
+                { '1', '1', '1' },
+                { '1', '1', '1' }
+        };
+        System.out.println(noi.numIslands(grid6));
+
+        /* Edge Case 5 - One Row Grid - Expected Output: 2 */
+        char[][] grid7 = { { '1', '0', '1' } };
+        System.out.println(noi.numIslands(grid7));
+
+        /* Edge Case 6 - One Column Grid - Expected Output: 2 */
+        char[][] grid8 = {
+                { '1' },
+                { '0' },
+                { '1' }
+        };
+        System.out.println(noi.numIslands(grid8));
+
+        /* Edge Case 7 - Diagonal Land - Expected Output: 3 */
+        char[][] grid9 = {
+                { '1', '0', '0' },
+                { '0', '1', '0' },
+                { '0', '0', '1' }
+        };
+        System.out.println(noi.numIslands(grid9));
+
+        /* Edge Case 8 - Multiple Disconnected Islands - Expected Output: 3 */
+        char[][] grid10 = {
+                { '1', '0', '1', '0' },
+                { '0', '1', '0', '1' },
+                { '1', '0', '1', '0' }
+        };
+        System.out.println(noi.numIslands(grid10));
 
     }
 }
