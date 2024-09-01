31735f9a8acfefba8f382dd3cefa6a67ad0583f7
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-01 22:23:01 +0100

    base for solution. didnt work with dfs, trying tomorrow with bfs

--- neetcode/graphs/islands_and_treasure/IslandsAndTreasure.java
+++ neetcode/graphs/islands_and_treasure/IslandsAndTreasure.java
@@ -1,5 +1,24 @@
 package neetcode.graphs.islands_and_treasure;
 
 public class IslandsAndTreasure {
-    
+    /*
+     * You are given a m×n 2D grid initialized with these three possible values:
+     * 
+     * -1 - A water cell that can not be traversed.
+     * 0 - A treasure chest.
+     * INF - A land cell that can be traversed. We use the integer 2^31 - 1 =
+     * 2147483647 to represent INF.
+     * Fill each land cell with the distance to its nearest treasure chest. If a
+     * land cell cannot reach a treasure chest then the value should remain INF.
+     * 
+     * Assume the grid can only be traversed up, down, left, or right.
+     */
+
+    private static final int WATER = -1;
+    private static final int TREASURE_CHEST = 0;
+    private static final int LAND = Integer.MAX_VALUE;
+
+    public void islandsAndTreasure(int[][] grid) {
+        // TODO: Finish solution
+    }
 }
