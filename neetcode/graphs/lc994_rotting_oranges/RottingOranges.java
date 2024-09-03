ae77beccb4ae0aa59c99ec7fb2965b1113fedc27
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-03 21:22:17 +0100

    Add RottingOranges class for solving the problem of rotting oranges in a grid | set up file

---
+++ neetcode/graphs/lc994_rotting_oranges/RottingOranges.java
@@ -0,0 +1,23 @@
+package neetcode.graphs.lc994_rotting_oranges;
+
+public class RottingOranges {
+    
+    /*
+     * You are given an m x n grid where each cell can have one of three values:
+     * 
+     *  0 representing an empty cell
+     *  1 representing a fresh orange
+     *  2 representing a rotten orange
+     * 
+     * every minute, any fresh orange that is 4 directionally adjacent to a rotten orange
+     * becomes rotten
+     * 
+     * return the minimum number of minutes that must elapse until no cell has a fresh orange.
+     * If this is impossible return -1
+     */
+
+
+    public int orangesRotting(int[][] grid) {
+
+    }
+}
