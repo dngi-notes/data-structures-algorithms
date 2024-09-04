20958310d0104034039befcd77f20b125d75cbb2
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-04 06:46:40 +0100

    set up file for solution

---
+++ leetcode-75/hash_maps_sets/lc1657_determine_if_two_strings_are_close/CloseStrings.java
@@ -0,0 +1,18 @@
+package hash_maps_sets.lc1657_determine_if_two_strings_are_close;
+
+public class CloseStrings {
+    /*
+     * Two strings are considered close if you can attain one from the other
+     * using the following operations:
+     * 
+     * - Operation 1: Swap any two existing characters.
+     * --> For example, abcde -> aecdb
+     * 
+     * - Operation 2: Transform every occurrence of one existing character
+     * into another existing character, and do the same with the other character
+     * --> For example, aacabb -> bbcbaa (all a's turn to b's and all b's to a's)
+     */
+
+    public boolean closeStrings(String word1, String word2) {
+    }
+}
