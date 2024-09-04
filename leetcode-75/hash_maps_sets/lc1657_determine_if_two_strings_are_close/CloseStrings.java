5c4e25a18aaea36a935c4e874244d7f3f11a7982
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-04 07:30:07 +0100

    attempted solution

--- leetcode-75/hash_maps_sets/lc1657_determine_if_two_strings_are_close/CloseStrings.java
+++ leetcode-75/hash_maps_sets/lc1657_determine_if_two_strings_are_close/CloseStrings.java
@@ -1,5 +1,9 @@
 package hash_maps_sets.lc1657_determine_if_two_strings_are_close;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
 public class CloseStrings {
     /*
      * Two strings are considered close if you can attain one from the other
@@ -13,7 +17,7 @@ public class CloseStrings {
      * --> For example, aacabb -> bbcbaa (all a's turn to b's and all b's to a's)
      */
 
-     public boolean closeStrings(String word1, String word2) {
+    public boolean closeStrings(String word1, String word2) {
         /* Strings need to be the same length for the operations to be possible */
 
         /*
@@ -23,7 +27,41 @@ public class CloseStrings {
          * check if the strings have the same set of numbers (keys in the map)
          * if they are equal return true, else return false
          * 
+         * Map out the frequency of the frequencies (to support operation 2)
+         * if the frequencies are equal, return true, else false
          */
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> word1Freqs = new HashMap<>();
+        Map<Character, Integer> word2Freqs = new HashMap<>();
+
+        for (char ch : word1.toCharArray()) {
+            word1Freqs.put(ch, word1Freqs.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : word2.toCharArray()) {
+            word2Freqs.put(ch, word2Freqs.getOrDefault(ch, 0) + 1);
+        }
+
+        Set<Character> word1Chars = word1Freqs.keySet();
+        Set<Character> word2Chars = word2Freqs.keySet();
+        if (!word1Chars.equals(word2Chars)) {
+            return false;
+        }
+
+        Map<Integer, Integer> word1FreqCount = new HashMap<>();
+        Map<Integer, Integer> word2FreqCount = new HashMap<>();
+
+        for(int freq: word1Freqs.values()) {
+            word1FreqCount.put(freq, word1FreqCount.getOrDefault(freq, 0) + 1);
+        }
+
+        for(int freq: word2Freqs.values()) {
+            word2FreqCount.put(freq, word2FreqCount.getOrDefault(freq, 0) + 1);
+        }
         
-     }
+        return word1FreqCount.equals(word2FreqCount);
+    }
 }
