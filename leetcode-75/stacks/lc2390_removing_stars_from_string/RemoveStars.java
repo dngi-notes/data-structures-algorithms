1bcf9cd315cfe70cba56daea4ea2176ef5529c9c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-05 11:17:18 +0100

    working solution

--- leetcode-75/stacks/lc2390_removing_stars_from_string/RemoveStars.java
+++ leetcode-75/stacks/lc2390_removing_stars_from_string/RemoveStars.java
@@ -1,5 +1,7 @@
 package stacks.lc2390_removing_stars_from_string;
 
+import java.util.Stack;
+
 public class RemoveStars {
     /*
      * You are given a string s, which contains '*'
@@ -15,7 +17,31 @@ public class RemoveStars {
      * The input will be generated such that the operation is always possible
      * It can be shown that the resulting string will always be unique
      */
-    public String removeString(String s) {
-        
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()) {
+            if(ch == '*') {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(ch);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(char ch: stack) {
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        RemoveStars rs = new RemoveStars();
+        String output = "Output: ";
+
+        String s1 = "leet**cod*e";
+        System.out.println(output + rs.removeStars(s1));
     }
 }
