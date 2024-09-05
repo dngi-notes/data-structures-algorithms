3338b32dbc43efd6402842092786a062310efc7b
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-05 10:54:22 +0100

    added class for solution

---
+++ leetcode-75/stacks/lc2390_removing_stars_from_string/RemoveStars.java
@@ -0,0 +1,21 @@
+package stacks.lc2390_removing_stars_from_string;
+
+public class RemoveStars {
+    /*
+     * You are given a string s, which contains '*'
+     * 
+     * In one operation, you can:
+     * -> choose a star in s
+     * -> remove the closest non-star character to its left, as well as remove the
+     * star itself
+     * 
+     * return the string after all stars have been removed.
+     * 
+     * Note:
+     * The input will be generated such that the operation is always possible
+     * It can be shown that the resulting string will always be unique
+     */
+    public String removeString(String s) {
+        
+    }
+}
