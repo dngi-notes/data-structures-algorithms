bf7387dae70784b0d1764a5c90cbaaaccfcb40f6
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-08 07:10:57 +0100

    solution + passed test case

---
+++ leetcode-75/linked_lists/lc2095_delete_the_middle_node_of_a_linked_list/DeleteMiddle.java
@@ -0,0 +1,70 @@
+package linked_lists.lc2095_delete_the_middle_node_of_a_linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode curr = this;
+        while(curr != null) {
+            sb.append(curr.val);
+            if(curr.next != null) {
+                sb.append("->");
+            }
+            curr = curr.next;
+        }
+        return sb.toString();
+    }
+}
+
+public class DeleteMiddle {
+    /*
+     * You are given the head of a linked list. Delete the middle node, and return
+     * the head of the modified linked list.
+     * 
+     * The middle node of a linked list of size n is the ⌊n / 2⌋th node from the
+     * start using 0-based indexing,
+     * where ⌊x⌋ denotes the largest integer less than or equal to x.
+     * 
+     * For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2,
+     * respectively.
+     */
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(prev != null) {
+            prev.next = slow.next;
+        }
+        
+        return head;
+    }
+
+    public static void main(String[] args) {
+        DeleteMiddle dm = new DeleteMiddle();
+        String output = "Output: ";
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println(output + dm.deleteMiddle(head).toString());
+    }
+}
