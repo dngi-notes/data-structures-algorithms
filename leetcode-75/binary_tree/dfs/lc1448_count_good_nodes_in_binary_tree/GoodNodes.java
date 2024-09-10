31b34015aa7aa7b3c0017c442ba891b08a14bb56
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-10 08:55:30 +0100

    updated solution (passes test cases)

--- leetcode-75/binary_tree/dfs/lc1448_count_good_nodes_in_binary_tree/GoodNodes.java
+++ leetcode-75/binary_tree/dfs/lc1448_count_good_nodes_in_binary_tree/GoodNodes.java
@@ -8,29 +8,34 @@ public class GoodNodes {
      * from root to X there are no nodes with a value greater than X.
      * 
      * Return the number of good nodes in the binary tree.
+     * 
+     * Constraints:
+     * 
+     * The number of nodes in the binary tree is in the range [1, 10^5].
+     * Each node's value is between [-10^4, 10^4].
      */
     public int goodNodes(TreeNode root) {
         /*
          * create a counter variable
-         * traverse the left and right of each node until null 
+         * traverse the left and right of each node until null
          * return the result of the search
          */
-        return countGoodNodes(root);
+        return countGoodNodes(root, -10000);
     }
-    
-    private int countGoodNodes(TreeNode node) {
-        if(node == null) {
+
+    private int countGoodNodes(TreeNode node, int maxVal) {
+        if (node == null) {
             return 0;
         }
 
-        int count = 1;
-
-        if(node.left.val >= node.val || node.right.val >= node.val) {
-            count++;
+        int count = 0;
+        if (node.val >= maxVal) {
+            count = 1;
+            maxVal = node.val;
         }
 
-        countGoodNodes(node.left);
-        countGoodNodes(node.right);
+        count += countGoodNodes(node.left, maxVal);
+        count += countGoodNodes(node.right, maxVal);
 
         return count;
     }
@@ -39,6 +44,11 @@ public class GoodNodes {
         GoodNodes gn = new GoodNodes();
         String output = "Output: ";
 
-        TreeNode root
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(3);
+        root1.right = null;
+        root1.left.left = new TreeNode(4);
+        root1.left.right = new TreeNode(2);
+        System.out.println(output + gn.goodNodes(root1));
     }
 }
