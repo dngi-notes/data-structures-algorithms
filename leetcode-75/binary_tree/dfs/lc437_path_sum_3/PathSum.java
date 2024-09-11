5a1b3e762ab457969d5bbdca7b7027aa250d6f02
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-11 06:10:23 +0100

    set up file for solution

---
+++ leetcode-75/binary_tree/dfs/lc437_path_sum_3/PathSum.java
@@ -0,0 +1,23 @@
+package binary_tree.dfs.lc437_path_sum_3;
+
+import data_structures.TreeNode;
+
+public class PathSum {
+    /*
+     * Given the root of a binary tree and an integer targetSum, return the number
+     * of paths where the sum of the values along the path equals targetSum.
+     * 
+     * The path does not need to start or end at the root or a leaf, but it must go
+     * downwards (i.e., traveling only from parent nodes to child nodes).
+     * 
+     * Constraints:
+     * 
+     * The number of nodes in the tree is in the range [0, 1000].
+     * -109 <= Node.val <= 109
+     * -1000 <= targetSum <= 1000
+     */
+    public int pathSum(TreeNode root, int targetSum) {
+
+    }
+
+}
