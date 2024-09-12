e815cb4ab6269b9e94c7eace675ec00eb64d9097
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-12 10:41:37 +0100

    added javadocs for TreeNode class

--- data_structures/TreeNode.java
+++ data_structures/TreeNode.java
@@ -1,17 +1,49 @@
 package data_structures;
 
+/**
+ * The {@code TreeNode} class represents a node in a binary tree.
+ * Each node contains an integer value, a reference to the left child, 
+ * and a reference to the right child.
+ * This class can be used to construct binary trees for various tree-based algorithms.
+ */
 public class TreeNode {
+    /**
+     * The value stored in the node.
+     */
     public int val;
+
+    /**
+     * The left child node.
+     */
     public TreeNode left;
+
+    /**
+     * The right child node.
+     */
     public TreeNode right;
 
+    /**
+     * Constructs an empty {@code TreeNode}.
+     */
     public TreeNode() {
     }
 
+    /**
+     * Constructs a {@code TreeNode} with the specified value and no children.
+     *
+     * @param val the value to be stored in the node
+     */
     public TreeNode(int val) {
         this.val = val;
     }
 
+    /**
+     * Constructs a {@code TreeNode} with the specified value and child nodes.
+     *
+     * @param val   the value to be stored in the node
+     * @param left  the left child node
+     * @param right the right child node
+     */
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
