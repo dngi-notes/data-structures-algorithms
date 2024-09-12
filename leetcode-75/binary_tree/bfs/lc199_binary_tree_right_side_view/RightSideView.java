ab01141b8996b4e7533b0ccb1b6b0e546e59d41b
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-12 10:41:00 +0100

    set up for binarytree/bfs solution

---
+++ leetcode-75/binary_tree/bfs/lc199_binary_tree_right_side_view/RightSideView.java
@@ -0,0 +1,10 @@
+package binary_tree.bfs.lc199_binary_tree_right_side_view;
+
+import data_structures.TreeNode;
+
+public class RightSideView {
+    /*
+     * Given the root of a binary tree, imagine yourself standing on the right side
+     * of it, return the values of the nodes you can see ordered from top to bottom.
+     */
+}
