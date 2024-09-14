8f1cbb26f5e27a2df908094c1faa475d885188d9
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-14 19:27:59 +0100

    initial solution

--- leetcode-75/graphs/dfs/lc841_keys_and_rooms/KeysAndRooms.java
+++ leetcode-75/graphs/dfs/lc841_keys_and_rooms/KeysAndRooms.java
@@ -2,6 +2,7 @@ package graphs.dfs.lc841_keys_and_rooms;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class KeysAndRooms {
     /*
@@ -18,6 +19,28 @@ public class KeysAndRooms {
      * otherwise.
      */
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        visited[0] = true;
+
+        while(!stack.isEmpty()) {
+            int currentRoom = stack.pop();
+            for(int key : rooms.get(currentRoom)) {
+                if(!visited[key]) {
+                    visited[key] = true;
+                    stack.push(key);
+                }
+            }
+        }
+
+        for(boolean roomVisited : visited) {
+            if(!roomVisited) {
+                return false;
+            }
+        }
+        return true;
     }
+
 }
