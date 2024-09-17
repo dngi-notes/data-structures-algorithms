ea5b97218a4534a0771e5494be7840df939c483c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-17 11:31:35 +0100

    optimised solution

--- leetcode-75/heap_priority_queue/lc215_kth_largest_element_in_array/FindKthLargest.java
+++ leetcode-75/heap_priority_queue/lc215_kth_largest_element_in_array/FindKthLargest.java
@@ -13,16 +13,22 @@ public class FindKthLargest {
      * Can you solve it without sorting?
      */
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
 
         for(int num : nums) {
             minHeap.offer(num);
-        }
-
-        while(minHeap.size() > k) {
-            minHeap.poll();
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
 
         return minHeap.peek();
+    }
+    public static void main(String[] args) {
+        FindKthLargest fkl = new FindKthLargest();
+        String output = "Output: ";
+
+        int[] nums1 = {1, 2, 3, 5, 4};
+        System.out.println(output + fkl.findKthLargest(nums1, 1));
     }
 }
