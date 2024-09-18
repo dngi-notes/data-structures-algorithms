f008fc3360d188fcd53e085807ef189e4a2daa90
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-18 08:07:48 +0100

    working solution | passes test cases on leetcode

--- leetcode-75/heap_priority_queue/lc2336_smallest_number_in_infinite_set/SmallestInfiniteSet.java
+++ leetcode-75/heap_priority_queue/lc2336_smallest_number_in_infinite_set/SmallestInfiniteSet.java
@@ -1,5 +1,9 @@
 package heap_priority_queue.lc2336_smallest_number_in_infinite_set;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class SmallestInfiniteSet {
     /*
      * You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
@@ -15,15 +19,29 @@ public class SmallestInfiniteSet {
      * void addBack(int num) Adds a positive integer num back into the infinite set,
      * if it is not already in the infinite set.
      */
-    public SmallestInfiniteSet() {
+    private PriorityQueue<Integer> minHeap;
+    private Set<Integer> set;
+    private int nextNumber;
 
+    public SmallestInfiniteSet() {
+        minHeap = new PriorityQueue<>();
+        set = new HashSet<>();
+        nextNumber = 1;
     }
 
     public int popSmallest() {
-        
+        if(minHeap.isEmpty()) {
+            return nextNumber++;
+        } else {
+            int smallest = minHeap.poll();
+            set.remove(smallest);
+            return smallest;
+        }
     }
 
-    public void addBack() {
-
+    public void addBack(int num) {
+        if(num < nextNumber && set.add(num)) {
+            minHeap.offer(num);
+        }
     }
 }
