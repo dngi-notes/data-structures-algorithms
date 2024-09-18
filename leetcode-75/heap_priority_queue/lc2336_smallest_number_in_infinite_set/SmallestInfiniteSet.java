fe0d01982afeda488b35d915d6e40d07907a7a3d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-18 07:12:19 +0100

    set up file for solution

---
+++ leetcode-75/heap_priority_queue/lc2336_smallest_number_in_infinite_set/SmallestInfiniteSet.java
@@ -0,0 +1,29 @@
+package heap_priority_queue.lc2336_smallest_number_in_infinite_set;
+
+public class SmallestInfiniteSet {
+    /*
+     * You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+     * 
+     * Implement the SmallestInfiniteSet class:
+     * 
+     * SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain
+     * all positive integers.
+     * 
+     * int popSmallest() Removes and returns the smallest integer contained in the
+     * infinite set.
+     * 
+     * void addBack(int num) Adds a positive integer num back into the infinite set,
+     * if it is not already in the infinite set.
+     */
+    public SmallestInfiniteSet() {
+
+    }
+
+    public int popSmallest() {
+        
+    }
+
+    public void addBack() {
+
+    }
+}
