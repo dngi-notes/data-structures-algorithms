81aeb2fb8051215399b61833997ea7e03826a5a5
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-20 09:13:03 +0100

    solution drawn up

--- leetcode-75/binary_search/lc162_find_peak_element/FindPeak.java
+++ leetcode-75/binary_search/lc162_find_peak_element/FindPeak.java
@@ -16,5 +16,27 @@ public class FindPeak {
      */
     public int findPeakElement(int[] nums) {
         
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        FindPeak fp = new FindPeak();
+        String output = "Output: ";
+
+        int[] nums1 = {1, 2, 1, 3, 5, 6, 4};
+        System.out.println(output + fp.findPeakElement(nums1));
     }
 }
