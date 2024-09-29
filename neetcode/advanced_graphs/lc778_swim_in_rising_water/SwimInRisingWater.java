a4bff342601f32c75a51f6ed7abc78299271bf3a
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-29 23:19:24 +0100

    passes all test cases

--- neetcode/advanced_graphs/lc778_swim_in_rising_water/SwimInRisingWater.java
+++ neetcode/advanced_graphs/lc778_swim_in_rising_water/SwimInRisingWater.java
@@ -69,5 +69,11 @@ public class SwimInRisingWater {
         int[][] grid2 = { { 0, 1, 2, 3, 4 }, { 24, 23, 22, 21, 5 }, { 12, 13, 14, 15, 16 }, { 11, 17, 18, 19, 20 },
                 { 10, 9, 8, 7, 6 } };
         System.out.println(output + sirw.swimInWater(grid2));
+
+        int[][] grid3 = null;
+        System.out.println(output + sirw.swimInWater(grid3));
+
+        int[][] grid4 = new int[0][0];
+        System.out.println(output + sirw.swimInWater(grid4));
     }
 }
