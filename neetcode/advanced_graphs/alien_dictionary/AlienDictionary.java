48ce83724ccafd5d377d5ee3855f26ca453d5e71
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-30 17:44:50 +0100

    initial solution + first testcase passed

--- neetcode/advanced_graphs/alien_dictionary/AlienDictionary.java
+++ neetcode/advanced_graphs/alien_dictionary/AlienDictionary.java
@@ -21,6 +21,77 @@ public class AlienDictionary {
      * There is no index i such that a[i] != b[i] and a.length < b.length.
      */
     public String alienDictionary(String[] words) {
+        // create an adjacency list and indegree array
+        Map<Character, List<Character>> graph = new HashMap<>();
+        Map<Character, Integer> inDegree = new HashMap<>();
 
+        // initialise indegree array/map for all unique characters from words
+        for(String word: words) {
+            for(char ch: word.toCharArray()) {
+                inDegree.putIfAbsent(ch, 0); // e.g. words = ["z", "o"] -> inDegree = {z: 0, o: 0}
+            }
+        }
+
+        // building the graph by comparing adjacent words
+        for(int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+
+            // edge case for invalid input
+            if(word1.length() > word2.length() && word2.startsWith(word1)) {
+                return "";
+            }
+
+            for(int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
+                char parent = word1.charAt(j);
+                char child = word2.charAt(j);
+                if(parent != child) {
+                    graph.computeIfAbsent(parent, key -> new ArrayList<>());
+                    if(!graph.get(parent).contains(child)) {
+                        graph.get(parent).add(child);
+                        inDegree.put(child, inDegree.get(child) + 1);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        Queue<Character> queue = new LinkedList<>();
+        StringBuilder result = new StringBuilder();
+
+        for(char ch : inDegree.keySet()) {
+            if(inDegree.get(ch) == 0) {
+                queue.add(ch);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            char currentChar = queue.poll();
+            result.append(currentChar);
+
+            if(graph.containsKey(currentChar)) {
+                for(char neighbor : graph.get(currentChar)) {
+                    inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+                    if (inDegree.get(neighbor) == 0) {
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        if(result.length() != inDegree.size()) {
+            return "";
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        AlienDictionary ad = new AlienDictionary();
+        String output = "Output: ";
+
+        String[] words1 = {"z", "o"};
+        System.out.println(output + ad.alienDictionary(words1));
     }
 }
