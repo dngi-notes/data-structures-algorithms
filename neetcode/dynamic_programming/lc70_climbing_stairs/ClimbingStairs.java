e420b54baf132a774a5928da2b51c35e896f11c0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-03 13:35:56 +0100

    test cases passing | edge case tests too

--- neetcode/dynamic_programming/lc70_climbing_stairs/ClimbingStairs.java
+++ neetcode/dynamic_programming/lc70_climbing_stairs/ClimbingStairs.java
@@ -7,6 +7,8 @@ public class ClimbingStairs {
      * Each time you can either climb 1 or 2 steps. In how many distinct ways can
      * you climb to the top?
      */
+
+    // intuition: fibonacci sequence
     public int climbStairs(int n) {
         if(n <= 1) {
             return 1;
@@ -22,5 +24,20 @@ public class ClimbingStairs {
         }
 
         return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs cs = new ClimbingStairs();
+        String output = "Output: ";
+
+        System.out.println(output + cs.climbStairs(2)); // expected output: 2    
+        System.out.println(output + cs.climbStairs(3)); // expected output: 3
+        System.out.println(output + cs.climbStairs(4)); // expected output: 5
+        System.out.println(output + cs.climbStairs(5)); // expected output: 5
+        
+        //edge cases
+        System.out.println(output + cs.climbStairs(1)); // expected output: 1
+        System.out.println(output + cs.climbStairs(0)); // expected output: 1
+
     }
 }
