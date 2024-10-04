c077f62f822ec014fa8fbefc3f2daa8713642125
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-04 17:24:02 +0100

    solution is passing leetcode test cases now

--- neetcode/dynamic_programming/lc746_min_cost_climbing_stairs/MinCostClimbingStairs.java
+++ neetcode/dynamic_programming/lc746_min_cost_climbing_stairs/MinCostClimbingStairs.java
@@ -12,7 +12,18 @@ public class MinCostClimbingStairs {
      * Return the minimum cost to reach the top of the floor.
      */
     public int minCostClimbingStairs(int[] cost) {
-        
+        int n = cost.length;
+        if(n == 0) return 0;
+        if(n == 1) return cost[0];
+
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        return Math.min(dp[n -1], dp[n - 2]);
     }
 
     public static void main(String[] args) {
@@ -21,5 +32,8 @@ public class MinCostClimbingStairs {
 
         int[] cost1 = {10, 15, 20};
         System.out.println(output + mccs.minCostClimbingStairs(cost1));
+
+        int[] cost2 = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        System.out.println(output + mccs.minCostClimbingStairs(cost2));
     }
 }
