a7f7a6821c7d5db322488383c2921871db984bd9
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-05 23:32:47 +0100

    attempted solution with approach

--- neetcode/dynamic_programming/lc198_house_robber/HouseRobber.java
+++ neetcode/dynamic_programming/lc198_house_robber/HouseRobber.java
@@ -13,6 +13,42 @@ public class HouseRobber {
      * police.
      */
     public int rob(int[] nums) {
+        int n = nums.length;
 
+        /*
+         * Edge Cases:
+         * 1. If there are no houses
+         * 2. If there's only one house
+         */
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+
+        int[] dp = new int[n];
+        /*
+         * Solving Sub-Problems (Base Cases):
+         * 1. The last house itself alone can be robbed (nums[n-1])
+         * 2. The second last house (nums[n-2]) can be robbed or the last house (nums[n-1]) (only if n is of size 2 or more)
+         */
+        dp[n - 1] = nums[n - 1];
+        if(n >= 2) {
+            dp[n - 2] = Math.max(nums[n - 1], nums[n - 2]);
+        }
+
+        /*
+         * Fill in the dp (memo array)
+         * starting at the third last, working down to the first
+         */
+        for(int i = n - 3; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        HouseRobber hr = new HouseRobber();
+        String output = "Output: ";
+
+        int[] nums1 = {1, 2, 3, 1};
+        System.out.println(output + hr.rob(nums1));
     }
 }
