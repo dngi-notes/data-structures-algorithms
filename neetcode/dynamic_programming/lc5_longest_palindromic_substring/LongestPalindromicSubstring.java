0f9ac954e0bb49db9d6e02f9aadfb905ef8cd6d2
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-08 19:40:12 +0100

    solution works now (properly handled the three subproblems)

--- neetcode/dynamic_programming/lc5_longest_palindromic_substring/LongestPalindromicSubstring.java
+++ neetcode/dynamic_programming/lc5_longest_palindromic_substring/LongestPalindromicSubstring.java
@@ -8,26 +8,48 @@ public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         int n = s.length();
 
-        if(n < 2) {
+        if (n < 2) {
             return s;
         }
 
-        boolean[] memo = new boolean[n];
+        boolean[][] memo = new boolean[n][n];
         int sIndex = 0;
         int maxLength = 1;
 
-        for(int j = 1; j < n; j++) {
-            for(int i = j; i >= 0; i--) {
-                if(s.charAt(j) == s.charAt(i)) {
-                    memo[i] = true;
+        /*
+         * Three Subproblems:
+         * 1. substrings of length 1 are always palindromes
+         * 2. substrings of length 2 are palindromes if the two characters are the same
+         * 3. substrings of length 3 are palindromes if the at least the left and right
+         * character are the same
+         */
 
-                    if(j - i + 1 > maxLength) {
-                        maxLength = j - i + 1;
+        // subproblem 1
+        for (int i = 0; i < n; i++) {
+            memo[i][i] = true;
+        }
+
+        // subproblem 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                memo[i][i + 1] = true;
+                sIndex = i;
+                maxLength = 2;
+            }
+        }
+
+        // subproblem 3
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+
+                if(s.charAt(i) == s.charAt(j) && memo[i + 1][j - 1]) {
+                    memo[i][j] = true;
+
+                    if(len > maxLength) {
                         sIndex = i;
+                        maxLength = len;
                     }
-
-                } else {
-                    memo[i] = false;
                 }
             }
         }
@@ -44,6 +66,6 @@ public class LongestPalindromicSubstring {
         System.out.println(output + lps.longestPalindrome("a")); // expected a
         System.out.println(output + lps.longestPalindrome("")); // expected ""
         System.out.println(output + lps.longestPalindrome("aacabdkacaa")); // expected "aca"
-        
+
     }
 }
