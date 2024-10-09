62d2044b3ccd9e4f083670e1285881df345bf00d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-09 23:32:23 +0100

    error in solution reset

--- neetcode/dynamic_programming/lc91_decode_ways/DecodeWays.java
+++ neetcode/dynamic_programming/lc91_decode_ways/DecodeWays.java
@@ -16,30 +16,13 @@ public class DecodeWays {
      * "26" -> 'Z'
      */
     public int numDecodings(String s) {
-        if(s == null || s.isEmpty()) {
-            return 0;
-        }
-
-        int n = s.length();
-        int[] memo = new int[n + 1];
-        memo[n] = 1;
-
-        for(int i = n - 1; i >= 0; i--) {
-            if(s.charAt(i) == '0') {
-                memo[i] = 0;
-            } else {
-                memo[i] = memo[i + 1];
-                if(s.charAt(i) == '1' || (s.charAt(i + 1) <= 6)) {
-                    memo[i] = memo[i + 2];
-                }
-            }
-        }
-        return memo[0];
+        
     }
 
     public static void main(String[] args) {
         DecodeWays dw = new DecodeWays();
         String output = "Output: ";
 
+        System.out.println(dw.numDecodings("12"));
     }
 }
