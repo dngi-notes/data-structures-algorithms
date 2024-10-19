f3687bafd63e2f53dc8e68bcdcd3a01ee780b760
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-19 07:53:29 +0100

    initial solution attempt

---
+++ neetcode/arrays_hashing/top_k_elements_in_list/TopKElements.java
@@ -0,0 +1,37 @@
+package neetcode.arrays_hashing.top_k_elements_in_list;
+
+import java.util.*;
+
+public class TopKElements {
+    /*
+     * Given an integer array nums and an integer k, return the k most frequent
+     * elements within the array.
+     * 
+     * The test cases are generated such that the answer is always unique.
+     * 
+     * You may return the output in any order.
+     */
+
+    public int[] topKFrequent(int[] nums, int k) {
+        /*
+         * get frequency of elements in nums (hashmap)
+         * add frequencies to a list sorted from highest to lowest
+         * loop over frequencies until k
+         * store those in a final result array 
+         */
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for(int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> sortedByFreq = new ArrayList<>(freqMap.keySet());
+        sortedByFreq.sort((a, b) -> freqMap.get(b) - freqMap.get(a));
+
+        int[] result = new int[k];
+        for(int i = 0; i < k; i++) {
+            result[i] = sortedByFreq.get(i);
+        }
+
+        return result;
+    }
+}
