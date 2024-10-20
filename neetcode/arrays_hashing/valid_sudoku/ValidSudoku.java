2d43b390cc3d4c47984faaf6ded2ad219692e9d0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-20 20:51:46 +0100

    set up for solution

---
+++ neetcode/arrays_hashing/valid_sudoku/ValidSudoku.java
@@ -0,0 +1,22 @@
+package neetcode.arrays_hashing.valid_sudoku;
+
+import java.util.*;
+
+public class ValidSudoku {
+    /*
+     * You are given a 9 x 9 Sudoku board `board`. 
+     * A Sudoku board is valid if the following rules are followed
+     * 
+     *  1. Each row must contain the digits `1-9` with duplicates
+     *  2. Each column must contain the digits `1-9` without duplicates
+     *  3. Each of the nine `3 x 3` sub-boxes of the grid must contain the digits 1-9 without duplicates
+     * 
+     * Return true if the Sudoku board is valid, otherwise return false
+     * 
+     * Note: A board doesn't need to be full or solvable to be valid
+     */
+
+     public boolean isValidSudoku(char[][] board) {
+        
+     }
+}
