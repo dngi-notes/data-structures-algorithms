d1f7f1d540818e6adf05374b698440758f79b5c0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-24 10:55:32 +0100

    tried solution again without help

--- neetcode/dynamic_programming/lc322_coin_change/CoinChange.java
+++ neetcode/dynamic_programming/lc322_coin_change/CoinChange.java
@@ -14,21 +14,18 @@ public class CoinChange {
      * You may assume that you have an unlimited number of each coin.
      */
     public int coinChange(int[] coins, int amount) {
-        /*
-         * dynamic programming solution
-         * - use a memo/dp array to store the minimum amount of coins needed to make up
-         * amount 'i'
-         */
-        int[] dp = new int[amount + 1];
-        dp[0] = 0; // it takes 0 coins to make up the amount 0
+        if(coins == null) {
+            return -1;
+        }
 
-        // fill the array's values with the worst case minimum (int max in this case)
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
         Arrays.fill(dp, 1, dp.length, Integer.MAX_VALUE);
 
-        // build from 1 to the target amount 'amount'
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+        for(int i = 1; i <= amount; i++) {
+            for(int coin : coins) {
+                if(i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
@@ -42,6 +39,17 @@ public class CoinChange {
         String output = "Output: ";
 
         int[] coins1 = { 1, 5, 10 };
-        System.out.println(output + cc.coinChange(coins1, 12));
+        System.out.println(output + cc.coinChange(coins1, 12)); // expected 3
+
+        int[] coins2 = {5, 10, 20};
+        System.out.println(output + cc.coinChange(coins2, 100)); // expected 5
+        System.out.println(output + cc.coinChange(coins2, 126)); // expected -1
+
+        int[] coins3 = {};
+        System.out.println(output + cc.coinChange(coins3, 10)); // expected -1
+        
+        int[] coins4 = null;
+        System.out.println(output + cc.coinChange(coins4, 10)); // expected -1
+        
     }
 }
