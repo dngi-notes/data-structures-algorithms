00d16483fe5adf788a6b89c59978756cde3a90fa
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-01 23:34:57 +0000

    fixed spacing

--- neetcode/dynamic_programming/lc746_min_cost_climbing_stairs/MinCostClimbingStairs.java
+++ neetcode/dynamic_programming/lc746_min_cost_climbing_stairs/MinCostClimbingStairs.java
@@ -22,7 +22,7 @@ public class MinCostClimbingStairs {
         for(int i = 2; i < n; i++) {
             dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
-        return Math.min(dp[n -1], dp[n - 2]);
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
 
     /*
