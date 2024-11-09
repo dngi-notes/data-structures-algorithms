dc2cc9ee41e5e070064fd1292c81b36fbfc0917e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-09 22:59:19 +0000

    set up problem description

---
+++ neetcode/linked_lists/lc2_add_two_numbers/AddNumbers.java
@@ -0,0 +1,16 @@
+package neetcode.linked_lists.lc2_add_two_numbers;
+
+import data_structures.ListNode;
+
+public class AddNumbers {
+    /*
+     * You are given two non-empty linked lists representing two non-negative
+     * integers. The digits are stored in reverse order, and each of their nodes
+     * contains a single digit. Add the two numbers and return the sum as a linked
+     * list.
+     * 
+     * You may assume the two numbers do not contain any leading zero, except the
+     * number 0 itself.
+     */
+    
+}
