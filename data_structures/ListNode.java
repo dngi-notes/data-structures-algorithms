0543fae95057d0a417719d2db95b4a03889df8b0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-12 06:05:08 +0000

    ListNode file cleanup

--- data_structures/ListNode.java
+++ data_structures/ListNode.java
@@ -11,7 +11,7 @@ public class ListNode {
     public ListNode() {}
 
     public ListNode(int val) {
-        this .val = val;
+        this.val = val;
     }
 
     public ListNode(int val, ListNode next) {
