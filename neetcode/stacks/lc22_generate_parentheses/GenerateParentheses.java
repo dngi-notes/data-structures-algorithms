80557d86b2af1fe0f9059e9af6175cf484176692
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-15 21:01:58 +0000

    basic set up complete

--- neetcode/stacks/lc22_generate_parentheses/GenerateParentheses.java
+++ neetcode/stacks/lc22_generate_parentheses/GenerateParentheses.java
@@ -1,5 +1,12 @@
 package neetcode.stacks.lc22_generate_parentheses;
+import java.util.*;
 
 public class GenerateParentheses {
-    
+    /*
+     * You are given an integer n. Return all well-formed parentheses strings that
+     * you can generate with n pairs of parentheses.
+     */
+    public List<List<String>> generateParentheses(int n) {
+
+    }
 }
