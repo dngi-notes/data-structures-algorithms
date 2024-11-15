cd5612ffb1884f972b94352ae1d4be1c60d136a8
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-15 21:28:43 +0000

    draft solution (stack / backtracking)

--- neetcode/stacks/lc22_generate_parentheses/GenerateParentheses.java
+++ neetcode/stacks/lc22_generate_parentheses/GenerateParentheses.java
@@ -1,4 +1,5 @@
 package neetcode.stacks.lc22_generate_parentheses;
+
 import java.util.*;
 
 public class GenerateParentheses {
@@ -6,7 +7,36 @@ public class GenerateParentheses {
      * You are given an integer n. Return all well-formed parentheses strings that
      * you can generate with n pairs of parentheses.
      */
-    public List<List<String>> generateParentheses(int n) {
+    public List<String> generateParentheses(int n) {
+        List<String> res = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+        backtrack(0, 0, n, res, stack);
 
+        return res;
+    }
+
+    private void backtrack(int open, int closed, int n, List<String> res, Stack<Character> stack) {
+        if (open == closed && open == n) {
+
+            StringBuilder sb = new StringBuilder();
+            for (char ch : stack) {
+                sb.append(ch);
+            }
+
+            res.add(sb.toString());
+            return;
+        }
+
+        if (open < closed) {
+            stack.push('(');
+            backtrack(open + 1, closed, n, res, stack);
+            stack.pop();
+        }
+
+        if (closed < open) {
+            stack.push(')');
+            backtrack(open, closed + 1, n, res, stack);
+            stack.pop();
+        }
     }
 }
