7ef2296b67706f61c6830a76cea9f11f7a4d9985
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-28 06:31:40 +0000

    first solution

--- neetcode/stacks/lc84_largest_rectangle_in_histogram/LargestRectangle.java
+++ neetcode/stacks/lc84_largest_rectangle_in_histogram/LargestRectangle.java
@@ -9,7 +9,17 @@ public class LargestRectangle {
         int maxArea = 0;
 
         for(int i = 0; i < heights.length; i++) {
-            
+            int currentHeight = (i == heights.length) ? 0 : heights[i];
+
+            while(!bars.isEmpty() && currentHeight < heights[bars.peek()]) {
+                int height = heights[bars.pop()];
+                int width = bars.isEmpty() ? i : i - bars.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+
+            bars.push(i);
         }
+
+        return maxArea;
     }
 }
