aed6a4ec62a633feb9e41078cdd3a397e0cc0b9e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-12-01 21:58:02 +0000

    if statement (finish later | gone to gym)

--- neetcode/binary_search/koko_eating_bananas/EatingBananas.java
+++ neetcode/binary_search/koko_eating_bananas/EatingBananas.java
@@ -3,5 +3,8 @@ package neetcode.binary_search.koko_eating_bananas;
 public class EatingBananas {
     public int minEatingSpeed(int[] piles, int h) {
 
+        if(piles == null || piles.length == 0) {
+            return 0;
+        }
     }
 }
